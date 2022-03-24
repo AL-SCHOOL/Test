892e5f187b7d29969387db72d900fed030e1cdcd
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2022-03-23 22:41:27 -0400

    Add files via upload

---
+++ LeeSportsTest/tabletennis.js
@@ -0,0 +1,16 @@
+// Functions
+function Darts() {location.href = "../Darts/darts.htm";}
+
+function TableTennis() {location.href = "tabletennis.htm";}
+
+function Adam() {location.href = "adam.htm";}
+
+function Erica() {location.href = "erica.htm";}
+
+function Karen() {location.href = "karen.htm";}
+
+function Anthony() {location.href = "anthony.htm";}
+
+// When Start
+document.getElementById("playoff_line").style.display = "none";
+document.getElementById("playoff").style.display = "none";
